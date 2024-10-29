67a1a7c26a6fb2f3fab731fa1762cd48272a0557
Dmitriy Latte <dimakhalaim@gmail.com>
2024-10-29 03:10:23 +0200

    user_state_service

--- src/data/services/logos_coffee_service/user_state_service.py
+++ src/data/services/logos_coffee_service/user_state_service.py
@@ -1,7 +1,9 @@
 from dataclasses import dataclass
 from src.data.services.logos_coffee_service.database import database
+from src.data.services.logos_coffee_service.models import UserState
 
 from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy import select
 
 @dataclass
 class UserData:
@@ -9,17 +11,39 @@ class UserData:
     user_id: int
     chat_id: int
 
-# async def __get_user_state_or_create(s:AsyncSession,    ):
+async def __create_user_state(s:AsyncSession, user_data: UserData):
+    user_state = UserState(bot_id = user_data.bot_id, user_id = user_data.user_id, chat_id = user_data.chat_id,
+                           state = None, data = dict())
+    s.add(user_state)
+    return user_state
 
+async def __get_user_state_or_create(s:AsyncSession, user_data: UserData):
+    res = await s.execute(select(UserState))
+    user_state = res.scalar_one_or_none()
+    if not user_state:
+        await __create_user_state(s, user_data)
+        res = await s.execute(select(UserState))
+        user_state = res.scalar_one_or_none()
+    return user_state
 
 async def set_state(user_data: UserData, state: str | None):
-    pass
+    async with database.session_factory() as session:
+        user_state = await __get_user_state_or_create(session, user_data)
+        user_state.state = state
+        await session.commit()
 
 async def get_state(user_data: UserData) -> str | None:
-    pass
+    async with database.session_factory() as session:
+        user_state = await __get_user_state_or_create(session, user_data)
+        return user_state.state
 
 async def set_data(user_data: UserData, data: dict):
-    pass
+    async with database.session_factory() as session:
+        user_state = await __get_user_state_or_create(session, user_data)
+        user_state.data = data
+        await session.commit()
 
 async def get_data(user_data: UserData) -> dict:
-    pass
+    async with database.session_factory() as session:
+        user_state = await __get_user_state_or_create(session, user_data)
+        return user_state.data
