a39eacd2cb283abf000efca2a8d14ea8a4964ab4
Dmitriy Latte <dimakhalaim@gmail.com>
2024-11-18 23:58:43 +0200

    Added: employee_service_impl.py

--- src/data/logoscoffee/interfaces/employee_service.py
+++ src/data/logoscoffee/interfaces/employee_service.py
@@ -1,6 +1,7 @@
 from abc import ABC, abstractmethod
+from datetime import datetime
 
-from src.data.logoscoffee.entities.orm_entities import EmployeeAccountEntity
+from src.data.logoscoffee.entities.orm_entities import EmployeeAccountEntity, OrderEntity
 
 
 class EmployeeService(ABC):
@@ -9,4 +10,26 @@ class EmployeeService(ABC):
     async def login(self, key: str | None) -> EmployeeAccountEntity:
         pass
 
+    @abstractmethod
+    async def get_new_orders(self, last_update_time: datetime) -> list[OrderEntity]:
+        pass
 
+    @abstractmethod
+    async def accept_order(self, order_id: int):
+        pass
+
+    @abstractmethod
+    async def cancel_order(self, order_id: int, cancel_details: str):
+        pass
+
+    @abstractmethod
+    async def start_cook_order(self, order_id: int):
+        pass
+
+    @abstractmethod
+    async def ready_order(self, order_id: int):
+        pass
+
+    @abstractmethod
+    async def complete_order(self, order_id: int):
+        pass
--- src/data/logoscoffee/services/employee_service_impl.py
+++ src/data/logoscoffee/services/employee_service_impl.py
@@ -1,12 +1,13 @@
 from loguru import logger
 
+from pydantic import TypeAdapter
 from sqlalchemy import select
 from sqlalchemy.exc import SQLAlchemyError
 
-from src.data.logoscoffee.entities.orm_entities import EmployeeAccountEntity
+from src.data.logoscoffee.entities.orm_entities import EmployeeAccountEntity, OrderEntity
 from src.data.logoscoffee.interfaces.employee_service import EmployeeService
 from src.data.logoscoffee.exceptions import *
-from src.data.logoscoffee.db.models import EmployeeAccountOrm
+from src.data.logoscoffee.db.models import EmployeeAccountOrm, OrderOrm
 from src.data.logoscoffee.session_manager import SessionManager
 
 
@@ -26,6 +27,99 @@ class EmployeeServiceImpl(EmployeeService):
         except InvalidKey as e:
             logger.warning(e)
             raise
+        except SQLAlchemyError as e:
+            logger.error(e)
+            raise DatabaseError(e)
+        except Exception as e:
+            logger.exception(e)
+            raise UnknownError(e)
+
+    async def get_new_orders(self, last_update_time: datetime) -> list[OrderEntity]:
+        try:
+            async with self.__session_manager.get_session() as s:
+                res = await s.execute(select(OrderOrm).filter(OrderOrm.date_pending >= last_update_time))
+                orders = res.scalars().all()
+
+                adapter = TypeAdapter(list[OrderEntity])
+                entities = adapter.validate_python(orders)
+
+                return entities
+        except SQLAlchemyError as e:
+            logger.error(e)
+            raise DatabaseError(e)
+        except Exception as e:
+            logger.exception(e)
+            raise UnknownError(e)
+
+    async def accept_order(self, order_id: int):
+        try:
+            async with self.__session_manager.get_session() as s:
+                res = await s.execute(select(OrderOrm).filter(OrderOrm.id == order_id))
+                order = res.scalars().first()
+
+                order.date_pending = datetime.now()
+                await s.commit()
+        except SQLAlchemyError as e:
+            logger.error(e)
+            raise DatabaseError(e)
+        except Exception as e:
+            logger.exception(e)
+            raise UnknownError(e)
+
+    async def cancel_order(self, order_id: int, cancel_details: str):
+        try:
+            async with self.__session_manager.get_session() as s:
+                res = await s.execute(select(OrderOrm).filter(OrderOrm.id == order_id))
+                order = res.scalars().first()
+
+                order.date_canceled = datetime.now()
+                order.cancel_details = cancel_details
+                await s.commit()
+        except SQLAlchemyError as e:
+            logger.error(e)
+            raise DatabaseError(e)
+        except Exception as e:
+            logger.exception(e)
+            raise UnknownError(e)
+
+    async def start_cook_order(self, order_id: int):
+        try:
+            async with self.__session_manager.get_session() as s:
+                res = await s.execute(select(OrderOrm).filter(OrderOrm.id == order_id))
+                order = res.scalars().first()
+
+                order.date_cooking = datetime.now()
+                await s.commit()
+        except SQLAlchemyError as e:
+            logger.error(e)
+            raise DatabaseError(e)
+        except Exception as e:
+            logger.exception(e)
+            raise UnknownError(e)
+
+    async def ready_order(self, order_id: int):
+        try:
+            async with self.__session_manager.get_session() as s:
+                res = await s.execute(select(OrderOrm).filter(OrderOrm.id == order_id))
+                order = res.scalars().first()
+
+                order.date_ready = datetime.now()
+                await s.commit()
+        except SQLAlchemyError as e:
+            logger.error(e)
+            raise DatabaseError(e)
+        except Exception as e:
+            logger.exception(e)
+            raise UnknownError(e)
+
+    async def complete_order(self, order_id: int):
+        try:
+            async with self.__session_manager.get_session() as s:
+                res = await s.execute(select(OrderOrm).filter(OrderOrm.id == order_id))
+                order = res.scalars().first()
+
+                order.date_completed = datetime.now()
+                await s.commit()
         except SQLAlchemyError as e:
             logger.error(e)
             raise DatabaseError(e)
