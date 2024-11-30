f14c090d8cab5cc34c6e8a0a43c51b626a7356b0
Alexey Flyagin <alexeyflyagin06@gmail.com>
2024-11-30 14:40:38 +0200

    Cleaning src\presentation\bots\employee_bot\handlers\utils.py

--- src/presentation/bots/employee_bot/handlers/utils.py
+++ src/presentation/bots/employee_bot/handlers/utils.py
@@ -1,12 +1,4 @@
-from datetime import datetime
-from decimal import Decimal
-from html import escape
-
 from aiogram import Bot
-from loguru import logger
-
-from aiogram.exceptions import TelegramBadRequest
-from aiogram.types import Message, URLInputFile
 
 from src.data.logoscoffee.entities.orm_entities import OrderEntity
 
